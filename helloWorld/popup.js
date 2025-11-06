3599f625b56afccbe32ed71a2393cf2c07ea3ab4
alexhsvm <alexhsvm@gmail.com>
2025-11-06 15:30:36 -0500

    Create Basic Chrome Extension

---
+++ helloWorld/popup.js
@@ -0,0 +1,28 @@
+// Helper to run code in the active tab
+function runInActiveTab(code) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        func: code
+      });
+    }
+  });
+}
+
+// Scroll Up
+document.getElementById("scrollUp").addEventListener("click", () => {
+  runInActiveTab(() => window.scrollBy(0, -500));
+});
+
+// Scroll Down
+document.getElementById("scrollDown").addEventListener("click", () => {
+  runInActiveTab(() => window.scrollBy(0, 500));
+});
+
+// Close Current Tab
+document.getElementById("closeTab").addEventListener("click", () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs[0]) chrome.tabs.remove(tabs[0].id);
+  });
+});
